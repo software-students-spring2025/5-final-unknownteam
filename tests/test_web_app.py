bfe8bbe9616d1e36cc319bd07cb9dc84f9b5477f
alw9411 <alw9411@nyu.edu>
2025-04-27 23:15:37 -0400

    fix test_web_app

--- tests/test_web_app.py
+++ tests/test_web_app.py
@@ -3,8 +3,7 @@ import os
 import pytest
 from unittest.mock import MagicMock, patch
 sys.path.append(os.path.abspath("./web-app"))
-import web_app.app as app
-import io
+import app as app
 
 #  package for creating mock mongoDB databases
 @pytest.fixture
@@ -28,10 +27,10 @@ def client():
         yield test_client
 
 @patch('app.requests.post')
-def test_upload(post, user_simulation):
+def test_upload(post):
     post.return_value = MagicMock(status_code=200)
     # Test with valid image file
-    response = user_simulation.post(
+    response = post(
         "/upload",
         data={
             "entry": 'Afghanistan'
@@ -41,9 +40,3 @@ def test_upload(post, user_simulation):
     )
     assert response.status_code == 200
     assert post.called
-    # Test with no entry
-    post.reset_mock()
-    response = user_simulation.post("/upload", follow_redirects=True)
-    assert b"Error: No text entry" in response.data
-    assert not post.called
-    
